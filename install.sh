a84d6b4d855588d6765ee2bd32e7315da9a6d169
devjk <rmsghk4254@naver.com>
2024-08-19 15:20:53 +0900

    feat: install script 보완

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-sudo ln -s $SCRIPT_DIR/run.sh /usr/local/bin/jarvis
+cd $SCRIPT_DIR
+npm install
+sudo ln -sf $SCRIPT_DIR/run.sh /usr/local/bin/jarvis
